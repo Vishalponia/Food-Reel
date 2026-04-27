9f2b0a194201863168d0bcef50ec931400a75893
vishalponia <vishalponiavp@gmail.com>
2026-04-27 20:07:16 +0530

    fix cookies for production

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -29,7 +29,11 @@ async function registerUser(req, res) {
         id: user._id,
     }, process.env.JWT_SECRET)
 
-    res.cookie("token", token)
+       res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+});
 
     res.status(201).json({
         message: "User registered successfully",
@@ -68,7 +72,11 @@ async function loginUser(req, res) {
         id: user._id,
     }, process.env.JWT_SECRET)
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+});
 
     res.status(200).json({
         message: "User logged in successfully",
@@ -117,8 +125,11 @@ async function registerFoodPartner(req, res) {
         id: foodPartner._id,
     }, process.env.JWT_SECRET)
 
-    res.cookie("token", token)
-
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+});
     res.status(201).json({
         message: "Food partner registered successfully",
         foodPartner: {
@@ -159,8 +170,11 @@ async function loginFoodPartner(req, res) {
         id: foodPartner._id,
     }, process.env.JWT_SECRET)
 
-    res.cookie("token", token)
-
+      res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+});
     res.status(200).json({
         message: "Food partner logged in successfully",
         foodPartner: {
