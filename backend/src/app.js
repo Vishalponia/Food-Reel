20bd5a747e13ebc8b5e3ca6d6a84f7d92471f009
vishalponia <vishalponiavp@gmail.com>
2026-04-27 19:51:04 +0530

    Connect with frontend successfully

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cookieParser())
 app.use(express.json());
 app.use(cors({
-  origin :"https://food-reel-murex.vercel.app/",
+  origin :"https://food-reel-murex.vercel.app",
   credentials:true,
 }));
 
